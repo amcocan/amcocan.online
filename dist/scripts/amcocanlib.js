4b6d744ae3c5aab0d7ba3e84c31ecda763edc7d4
AMCocan <AMCocan@student.fullsail.edu>
2024-10-08 07:17:44 -0400

    Create 'amcocanlib' file

---
+++ dist/scripts/amcocanlib.js
@@ -0,0 +1,10 @@
+// Property of amcocan
+//
+// This will be a library of functions I will often use.
+// Not all the functions found here will be in use for every project.
+//
+// Here is a list of all the functions activly in use for this project.
+//
+// Active Functions:
+// - none
+
