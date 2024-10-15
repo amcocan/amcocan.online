cfc305b08abdbff51d7e4341d72c69d8770e8160
AMCocan <AMCocan@student.fullsail.edu>
2024-10-15 04:12:38 -0400

    Resume assembly start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
-  theme: {
-    extend: {},
+  theme:
+  {
+    fontFamily:
+    {
+      sans: ['Calibri', 'sans-serif'],
+    },
+    extend:
+    {
+      colors:
+      {
+        'crimson': '#FF004A',
+        'black': '#000000'
+      },
+      
+      borderRadius:
+      {
+        // '4xl': '2rem',
+      }
+    },
   },
   plugins: [],
 }
